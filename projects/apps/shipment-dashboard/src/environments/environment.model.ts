8bbedc14093ae356fc78911497802fbd66e93d33
Jeremiah <jeremiahkoeiman1@gmail.com>
2025-02-12 15:59:03 +0100

    add environment variables

---
+++ projects/apps/shipment-dashboard/src/environments/environment.model.ts
@@ -0,0 +1,7 @@
+export interface Environment {
+  production: boolean;
+  apiBaseUrl: string;
+  companyId: string;
+  brandId: string;
+  shipmentReference: string;
+}
--- projects/apps/shipment-dashboard/src/environments/environment.prod.ts
+++ projects/apps/shipment-dashboard/src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
-export const environment = {
+import { Environment } from './environment.model';
+
+export const environment: Environment = {
   production: true,
+  apiBaseUrl: 'https://api.pakketdienstqls.nl',
+  companyId: '9e606e6b-44a4-4a4e-a309-cc70ddd3a103',
+  brandId: 'e41c8d26-bdfd-4999-9086-e5939d67ae28',
+  shipmentReference: 'frontend-assessment'
 };
