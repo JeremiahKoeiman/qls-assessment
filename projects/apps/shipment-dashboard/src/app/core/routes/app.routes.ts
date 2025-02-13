89d6e0c20686baa4ac86e2623f6c8401e93632e7
Jeremiah <jeremiahkoeiman1@gmail.com>
2025-02-13 15:07:16 +0100

    update routes

--- projects/apps/shipment-dashboard/src/app/core/routes/app.routes.ts
+++ projects/apps/shipment-dashboard/src/app/core/routes/app.routes.ts
@@ -2,57 +2,19 @@ import { Route } from '@angular/router';
 import { languageScopeResolver } from '@qls/utilities/i18n';
 
 import { AppWrapperComponent } from '#sd/app/components/app-wrapper/app-wrapper.component';
-import { LoginContainer } from '#sd/app/components/login/login.container';
 import { OverviewComponent } from '#sd/app/components/overview/overview.component';
 import { PageNotFoundComponent } from '#sd/app/components/page-not-found/page-not-found.page';
 
-// export const appRoutes: Route[] = [
-//   {
-//     path: '',
-//     resolve: {
-//       translationScope: languageScopeResolver
-//     },
-//     component: LoginContainer,
-//     children: [
-//       {
-//         path: '',
-//         component: AppWrapperComponent,
-//         children: [
-//           {
-//             path: 'overview',
-//             component: OverviewComponent
-//           }
-//         ],
-//         canActivate: [AuthenticationGuard]
-//       }
-//     ]
-//   },
-//   {
-//     path: '**',
-//     component: PageNotFoundComponent
-//   }
-// ];
-
 export const appRoutes: Route[] = [
-  {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
-  },
-  {
-    path: 'login',
-    component: LoginContainer
-  },
   {
     path: '',
     resolve: {
       translationScope: languageScopeResolver
     },
     component: AppWrapperComponent,
-    // canActivate: [AuthenticationGuard],
     children: [
       {
-        path: 'dashboard',
+        path: '',
         component: OverviewComponent
       }
     ]
