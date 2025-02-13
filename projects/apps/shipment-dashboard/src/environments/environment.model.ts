be45123790d092223526198b6068cb85c9a317db
Jeremiah <jeremiahkoeiman1@gmail.com>
2025-02-13 14:41:35 +0100

    update environment files

--- projects/apps/shipment-dashboard/src/environments/environment.model.ts
+++ projects/apps/shipment-dashboard/src/environments/environment.model.ts
@@ -4,4 +4,5 @@ export interface Environment {
   companyId: string;
   brandId: string;
   shipmentReference: string;
+  bearerToken: string;
 }
--- projects/apps/shipment-dashboard/src/environments/environment.prod.ts
+++ projects/apps/shipment-dashboard/src/environments/environment.prod.ts
@@ -1,9 +1,10 @@
 import { Environment } from './environment.model';
 
 export const environment: Environment = {
-  production: true,
-  apiBaseUrl: 'https://api.pakketdienstqls.nl',
+  production: false,
+  apiBaseUrl: 'https://assignment.qls.nl',
   companyId: '9e606e6b-44a4-4a4e-a309-cc70ddd3a103',
   brandId: 'e41c8d26-bdfd-4999-9086-e5939d67ae28',
-  shipmentReference: 'frontend-assessment'
+  shipmentReference: 'frontend-assessment',
+  bearerToken: 'eyJHhusicuy8w374yrhfuijcoe09a8ya7hu4itmof87&^&FGTyuh9837ygr2bufhnciu98e7wg3yu4bhrnfiosjc89hunejwio3gy7fbeuvhiu'
 };
