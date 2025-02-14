7ead7aa03bff02fc57bd01cc27f401c48faf565c
Jeremiah <jeremiahkoeiman1@gmail.com>
2025-02-14 10:47:33 +0100

    add suffix to base url

--- projects/apps/shipment-dashboard/src/environments/environment.prod.ts
+++ projects/apps/shipment-dashboard/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { Environment } from './environment.model';
 
 export const environment: Environment = {
   production: false,
-  apiBaseUrl: 'https://assignment.qls.nl',
+  apiBaseUrl: 'https://assignment.qls.nl/api/frontend',
   companyId: '9e606e6b-44a4-4a4e-a309-cc70ddd3a103',
   brandId: 'e41c8d26-bdfd-4999-9086-e5939d67ae28',
   shipmentReference: 'frontend-assessment',
