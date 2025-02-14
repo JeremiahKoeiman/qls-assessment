ea668b3b89d39bc9041167eef600c99b5a2adbf5
Jeremiah <jeremiahkoeiman1@gmail.com>
2025-02-14 14:07:54 +0100

    lazy load components

--- projects/apps/shipment-dashboard/src/app/core/routes/app.routes.ts
+++ projects/apps/shipment-dashboard/src/app/core/routes/app.routes.ts
@@ -1,26 +1,22 @@
 import { Route } from '@angular/router';
 import { languageScopeResolver } from '@qls/utilities/i18n';
 
-import { AppWrapperComponent } from '#sd/app/components/app-wrapper/app-wrapper.component';
-import { OverviewComponent } from '#sd/app/components/overview/overview.component';
-import { PageNotFoundComponent } from '#sd/app/components/page-not-found/page-not-found.page';
-
 export const appRoutes: Route[] = [
   {
     path: '',
     resolve: {
       translationScope: languageScopeResolver
     },
-    component: AppWrapperComponent,
+    loadComponent: () => import('#sd/app/components/app-wrapper/app-wrapper.component').then(c => c.AppWrapperComponent),
     children: [
       {
         path: '',
-        component: OverviewComponent
+        loadComponent: () => import('#sd/app/components/overview/overview.container').then(c => c.OverviewComponent)
       }
     ]
   },
   {
     path: '**',
-    component: PageNotFoundComponent
+    loadComponent: () => import('#sd/app/components/page-not-found/page-not-found.page').then(c => c.PageNotFoundComponent)
   }
 ];
