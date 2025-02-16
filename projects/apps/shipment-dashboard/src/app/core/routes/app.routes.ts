4241653f444c3524de0d38712c7dd8cdc9509782
Jeremiah <jeremiahkoeiman1@gmail.com>
2025-02-16 18:21:21 +0100

    create detail component

--- projects/apps/shipment-dashboard/src/app/core/routes/app.routes.ts
+++ projects/apps/shipment-dashboard/src/app/core/routes/app.routes.ts
@@ -11,7 +11,15 @@ export const appRoutes: Route[] = [
     children: [
       {
         path: '',
+        loadComponent: () => import('#sd/app/features/components/welcome/welcome.component').then(c => c.WelcomeComponent)
+      },
+      {
+        path: 'shipments',
         loadComponent: () => import('#sd/app/features/containers/overview/overview.container').then(c => c.OverviewComponent)
+      },
+      {
+        path: 'shipments/:id',
+        loadComponent: () => import('#sd/app/features/containers/detail/detail.container').then(c => c.DetailComponent)
       }
     ]
   },
@@ -20,3 +28,33 @@ export const appRoutes: Route[] = [
     loadComponent: () => import('#sd/app/features/pages/page-not-found/page-not-found.page').then(c => c.PageNotFoundComponent)
   }
 ];
+
+// ---------------
+// export const appRoutes: Route[] = [
+//   {
+//     path: '',
+//     resolve: {
+//       translationScope: languageScopeResolver
+//     },
+//     redirectTo: 'shipments',
+//     pathMatch: 'full'
+//   },
+//   {
+//     path: 'shipments',
+//     loadComponent: () => import('#sd/app/features/components/app-wrapper/app-wrapper.component').then(c => c.AppWrapperComponent),
+//     children: [
+//       {
+//         path: '',
+//         loadComponent: () => import('#sd/app/features/containers/overview/overview.container').then(c => c.OverviewComponent)
+//       }
+//     ]
+//   },
+//   {
+//     path: 'shipments/:id',
+//     loadComponent: () => import('#sd/app/features/containers/detail/detail.container').then(c => c.DetailComponent)
+//   },
+//   {
+//     path: '**',
+//     loadComponent: () => import('#sd/app/features/pages/page-not-found/page-not-found.page').then(c => c.PageNotFoundComponent)
+//   }
+// ];
